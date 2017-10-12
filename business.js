90b070489e8f37d8635a2b8b5fa58155488a9093
Emma Kuo <emma.c.kuo@gmail.com>
2017-10-11 20:15:35 -0700

    more buziness value

--- business.js
+++ business.js
@@ -7,7 +7,25 @@ function provideValue(a, b) {
     return (a * b).toString()
 }
 
+function synergize(a){
+    var result = 1
+    
+    if(a instanceof Array)
+    {
+        a.forEach(function(element) 
+        {
+            result *= element
+        })
+    } else {
+        result = a
+    }
+    return result.toString()
+}
+
 module.exports = {
     'go': go,
-    'provideValue': provideValue
+    'provideValue': provideValue,
+    'synergize': synergize
 }
+
+
--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.get('/go/:a/:b', function (req, res) {
       res.send(business.go(Number(req.params['a']), Number(req.params.b)))
 })
 
+app.get('/provideValue/:a/:b', function (req, res) {
+      res.send(business.provideValue(Number(req.params['a']), Number(req.params.b)))
+})
+
+app.get('/synergize/', function( req, res){
+      res.send(business.synergize(req.query.a))
+})
+
 app.listen(port, function () {
       console.log('listening on port', port)
 })
